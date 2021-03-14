099f2716afb8b7f532138dc4bdf39234f51fc419
Evsikov <hikarokayto5@gmail.com>
2021-03-14 22:47:18 +0300

    add adaptive category page

--- components/category/Category.js
+++ components/category/Category.js
@@ -3,39 +3,31 @@ import classes from './Category.module.scss'
 import Link from "next/link";
 
 const Category = () => {
+
+    const data = [
+        {id: 1, title: 'Евгения & Евгений', img: '/static/images/category/wedding/1.jpg'},
+        {id: 2, title: 'Анастасия & Олег', img: '/static/images/category/wedding/2.jpg'},
+        {id: 3, title: 'Анастасия & Алексей', img: '/static/images/category/wedding/3.jpg'},
+        {id: 4, title: 'Алёна & Дмитрий', img: '/static/images/category/wedding/4.jpg'},
+    ]
+
     return (
-       <section className={classes.section}>
-           <div className={classes.title}>
-               <h1>Свадебная фотосессия</h1>
-               <Link href={'/portfolio'}><a className={classes.back}>Вернуться назад</a></Link>
-           </div>
-           <div className={classes.block}>
-               <Link href={'/portfolio/work/1'}>
-                   <a className={classes.item}>
-                       <h2>Евгения & Евгений</h2>
-                       <img src="/static/images/category/wedding/1.jpg" alt=""/>
-                   </a>
-               </Link>
-               <Link href={'/portfolio/work/1'}>
-                   <a className={classes.item}>
-                       <h2>Анастасия & Олег</h2>
-                       <img src="/static/images/category/wedding/2.jpg" alt=""/>
-                   </a>
-               </Link>
-               <Link href={'/portfolio/work/1'}>
-                   <a className={classes.item}>
-                       <h2>Анастасия & Алексей</h2>
-                       <img src="/static/images/category/wedding/3.jpg" alt=""/>
-                   </a>
-               </Link>
-               <Link href={'/portfolio/work/1'}>
-                   <a className={classes.item}>
-                       <h2>Алёна & Дмитрий</h2>
-                       <img src="/static/images/category/wedding/4.jpg" alt=""/>
-                   </a>
-               </Link>
-           </div>
-       </section>
+        <>
+            <div className={classes.title}>
+                <h1>Свадебная фотосессия</h1>
+                <Link href={'/portfolio'}><a className={classes.back}>Вернуться назад</a></Link>
+            </div>
+            <div className={classes.block}>
+                {data.map(item => (
+                    <Link href={`/portfolio/work/${item.id}`} key={item.id}>
+                        <a className={classes.item}>
+                            <h2>{item.title}</h2>
+                            <img src={item.img} alt=""/>
+                        </a>
+                    </Link>
+                ))}
+            </div>
+        </>
     )
 }
 
